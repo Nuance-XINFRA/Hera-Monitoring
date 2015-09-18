64dbcf0b48fa9c414dfdb21f022e7796ae287d65
fares <fares.jaradeh@nuance.com>
2015-09-18 18:22:55 -0400

    fix issue with Grafana-2-Elasticsearch comunication related to Cross-origin resource sharing (CORS).....part1

--- build/grafana/grafana_init_script.sh
+++ build/grafana/grafana_init_script.sh
@@ -16,59 +16,64 @@ function logInfo ( )
 }
 
 #######################################################
-# replace default settings in Hera config.js to point to loclahost elasticsearch
-# NOTE: in your docker-compose, make sure the env variable HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL is set to a valid url (eg. "http://" + window.location.hostname + ":9200" )
-#       ie HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL="\"http://\" + window.location.hostname + \":9200\""
+# replace default settings in Hera config.js to point to localhost elasticsearch
+# NOTE: in your docker-compose, make sure the env variable HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT is set to a valid port
+#       ie HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT="9200"
 #######################################################
 function replaceGrafanaElasticSearchUrlSettings ( )
 {
 	logInfo "calling replaceGrafanaElasticSearchUrlSettings" 
 	
-	# chech if the HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL environment variable is set
-	if [ -z "$HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL" ]; then
-		logInfo "WARNING: \$HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL env varialble is not set, this should have been done in the docker-compose.yml. nothing to do here!"
+	# chech if the HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT environment variable is set
+	if [ -z "$HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT" ]; then
+		logInfo "WARNING: \$HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT env varialble is not set, this should have been done in the docker-compose.yml. nothing to do here!"
 	else
-		logInfo "\$HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL is ${HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL}"   
+		logInfo "\$HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT is ${HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT}"   
 		# remove white spaces
-		HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL_NO_WHITE_SPACES="$(echo -e "${HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL}" | tr -d '[[:space:]]')"
-		logInfo "\$HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL_NO_WHITE_SPACES is ${HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL_NO_WHITE_SPACES}"   
+		HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT_NO_WHITE_SPACES="$(echo -e "${HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT}" | tr -d '[[:space:]]')"
+		logInfo "\$HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT_NO_WHITE_SPACES is ${HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT_NO_WHITE_SPACES}"   
 		# check how to interpret the value
-		if [ ${HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL_NO_WHITE_SPACES} == "" ]; then 			
-			logInfo "WARNING: \$HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL env varialble is set to empty, this should have been set to a valid url (eg. (eg. \"http://\" + window.location.hostname + \":9200\" ))  in the docker-compose.yml. nothing to do here!"
+		if [ ${HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT_NO_WHITE_SPACES} == "" ]; then 			
+			logInfo "WARNING: \$HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT env varialble is set to empty, this should have been set to a valid port (eg. \"9200\")  in the docker-compose.yml. nothing to do here!"
 		else
 			# TODO add further checking in future
-			# substitute values in config.js
-			sed -i "/\/elasticsearch\//c\url: ${HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL}," /usr/share/grafana/config.js
-			#TODO-test# sed -i "/\/elasticsearch\//c\url: ${HERA_GRAFANA_LOCAL_ELASTICSEARCH_URL}," grafana/config.js
+			# substitute values in config.js (must use cp -f....device may be locked)
+			cp -f /usr/share/grafana/config.js /usr/share/grafana/config-grafana-init-script-backup.js
+			sed -i "s_url: \"https://\" + window.location.hostname + \"/elasticsearch/\"_url: \"http://\" + window.location.host + \"/elasticsearch\"_" /usr/share/grafana/config-grafana-init-script-backup.js
+			cp -f /usr/share/grafana/config-grafana-init-script-backup.js /usr/share/grafana/config.js 
+			# create redirect in apache2 conf to avoid the CORS issues (redirects from grafana java_script)
+			echo "ProxyPass /elasticsearch/ http://elasticsearch:${HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT_NO_WHITE_SPACES}/" >> /etc/apache2/apache2.conf
+			echo "ProxyPassReverse /elasticsearch/ http://elasticsearch:${HERA_GRAFANA_LOCAL_ELASTICSEARCH_PORT_NO_WHITE_SPACES}/" >> /etc/apache2/apache2.conf
 		fi
 	fi	
 }
 
 #######################################################
-# replace default settings in Hera config.js to point to loclahost graphite
-# NOTE: in your docker-compose, make sure the env variable HERA_GRAFANA_LOCAL_GRAPHITE_URL is set to a valid url (eg. "http://" + window.location.hostname + ":9200" )
-#       ie HERA_GRAFANA_LOCAL_GRAPHITE_URL="\"http://\" + window.location.hostname + \":80\""
+# replace default settings in Hera config.js to point to localhost graphite
+# NOTE: in your docker-compose, make sure the env variable HERA_GRAFANA_LOCAL_GRAPHITE_PORT is set to a valid port
+#       ie HERA_GRAFANA_LOCAL_GRAPHITE_PORT="80"
 #######################################################
 function replaceGrafanaGraphiteUrlSettings ( )
 {
 	logInfo "calling replaceGrafanaGraphiteUrlSettings" 
 	
-	# chech if the HERA_GRAFANA_LOCAL_GRAPHITE_URL environment variable is set
-	if [ -z "$HERA_GRAFANA_LOCAL_GRAPHITE_URL" ]; then
-		logInfo "WARNING: \$HERA_GRAFANA_LOCAL_GRAPHITE_URL env varialble is not set, this should have been done in the docker-compose.yml. nothing to do here!"
+	# chech if the HERA_GRAFANA_LOCAL_GRAPHITE_PORT environment variable is set
+	if [ -z "$HERA_GRAFANA_LOCAL_GRAPHITE_PORT" ]; then
+		logInfo "WARNING: \$HERA_GRAFANA_LOCAL_GRAPHITE_PORT env varialble is not set, this should have been done in the docker-compose.yml. nothing to do here!"
 	else
-		logInfo "\$HERA_GRAFANA_LOCAL_GRAPHITE_URL is ${HERA_GRAFANA_LOCAL_GRAPHITE_URL}"   
+		logInfo "\$HERA_GRAFANA_LOCAL_GRAPHITE_PORT is ${HERA_GRAFANA_LOCAL_GRAPHITE_PORT}"   
 		# remove white spaces
-		HERA_GRAFANA_LOCAL_GRAPHITE_URL_NO_WHITE_SPACES="$(echo -e "${HERA_GRAFANA_LOCAL_GRAPHITE_URL}" | tr -d '[[:space:]]')"
-		logInfo "\$HERA_GRAFANA_LOCAL_GRAPHITE_URL_NO_WHITE_SPACES is ${HERA_GRAFANA_LOCAL_GRAPHITE_URL_NO_WHITE_SPACES}"   
+		HERA_GRAFANA_LOCAL_GRAPHITE_PORT_NO_WHITE_SPACES="$(echo -e "${HERA_GRAFANA_LOCAL_GRAPHITE_PORT}" | tr -d '[[:space:]]')"
+		logInfo "\$HERA_GRAFANA_LOCAL_GRAPHITE_PORT_NO_WHITE_SPACES is ${HERA_GRAFANA_LOCAL_GRAPHITE_PORT_NO_WHITE_SPACES}"   
 		# check how to interpret the value
-		if [ ${HERA_GRAFANA_LOCAL_GRAPHITE_URL_NO_WHITE_SPACES} == "" ]; then 			
-			logInfo "WARNING: \$HERA_GRAFANA_LOCAL_GRAPHITE_URL env varialble is set to empty, this should have been set to a valid url (eg. (eg. \"http://\" + window.location.hostname + \":9200\" ))  in the docker-compose.yml. nothing to do here!"
+		if [ ${HERA_GRAFANA_LOCAL_GRAPHITE_PORT_NO_WHITE_SPACES} == "" ]; then 			
+			logInfo "WARNING: \$HERA_GRAFANA_LOCAL_GRAPHITE_PORT env varialble is set to empty, this should have been set to a valid port (eg. \"80\")  in the docker-compose.yml. nothing to do here!"
 		else
 			# TODO add further checking in future
-			# substitute values in config.js
-			sed -i "/\/graphite\//c\url: ${HERA_GRAFANA_LOCAL_GRAPHITE_URL}," /usr/share/grafana/config.js
-			#TODO-test# sed -i "/\/graphite\//c\url: ${HERA_GRAFANA_LOCAL_GRAPHITE_URL}," grafana/config.js
+			# substitute values in config.js (must use cp -f....device may be locked)
+			cp -f /usr/share/grafana/config.js /usr/share/grafana/config-grafana-init-script-backup.js
+			sed -i "s_url: \"https://\" + window.location.hostname + \"/graphite/\"_url: \"http://\" + window.location.hostname + \":${HERA_GRAFANA_LOCAL_GRAPHITE_PORT_NO_WHITE_SPACES}\"_" /usr/share/grafana/config-grafana-init-script-backup.js
+			cp -f /usr/share/grafana/config-grafana-init-script-backup.js /usr/share/grafana/config.js
 		fi
 	fi	
 }
@@ -78,10 +83,10 @@ function replaceGrafanaGraphiteUrlSettings ( )
 #######################################################
 logInfo 'START GRAFANA INIT CONFIG SCRIPT!!'
 
-# replace the default Hera settings in config.js to point to loclahost elasticsearch
+# replace the default Hera settings in config.js & apache2.conf to point to loclahost elasticsearch
 replaceGrafanaElasticSearchUrlSettings
 
-# replace the default Hera settings in config.js to point to loclahost elasticsearch
+# replace the default Hera settings in config.js & apache2.conf to point to loclahost graphite
 replaceGrafanaGraphiteUrlSettings
 
 logInfo 'END GRAFANA INIT CONFIG SCRIPT!!'
